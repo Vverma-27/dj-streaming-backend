828df23af739a3ee76a8d1821dc1261e9b979f31
Vihaan Verma <vverma270705@gmail.com>
2021-08-01 22:39:05 +0530

    new route

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,8 +1,19 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async me(ctx) {
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.badRequest(null, [{ message: "No Autharization Header" }]);
+    }
+    const events = await strapi.services.events.find({ user: user.id });
+    if (!events) return ctx.notFound();
+    return sanitizeEntity(events, { model: strapi.models.events });
+  },
+};
